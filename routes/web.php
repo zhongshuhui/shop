ce1752c0e23e55ded5c454765420100f7dceca41
zhongshuhui <1003487655@qq.com>
2023-10-19 15:33:47 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
