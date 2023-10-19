230866c3d339b05e84870a1b21cd9d1933743d7b
zhongshuhui <1003487655@qq.com>
2023-10-19 15:12:03 +0800

    注册与登录

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use \App\Http\Controllers\PagesController;
 
 #Route::get('/', [PagesController::class,'root'])->name('root');
 Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
